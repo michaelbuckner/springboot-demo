13bbff487656194eaff179553005bfcf050bd37e
mike <mike@servicenow.com>
2018-08-19 14:03:23 -0400

    first commit

---
+++ src/main/java/com/itom/nextGenDemo/Application.java
@@ -0,0 +1,36 @@
+package com.itom.nextGenDemo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.itom.nextGenDemo.bookmarks.Account;
+import com.itom.nextGenDemo.bookmarks.AccountRepository;
+import com.itom.nextGenDemo.bookmarks.Bookmark;
+import com.itom.nextGenDemo.bookmarks.BookmarkRepository;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(AccountRepository accountRepository,
+	                       BookmarkRepository bookmarkRepository) {
+		return args ->
+				Arrays.asList("jhoeller","dsyer","pwebb","ogierke","rwinch","mfisher","mpollack","jlong")
+						.forEach(username -> {
+							Account account = accountRepository.save(new Account(username, "password"));
+							bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + username, "A description"));
+							bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + username, "A description"));
+						});
+	}
+
+}
---
+++ src/test/java/com/itom/nextGenDemo/NextGenDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itom.nextGenDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NextGenDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
